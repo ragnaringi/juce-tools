57ca1944c5013c02df1e64d6039417d79ae9ea50
Ragnar Hrafnkelsson <ragnar.hrafnkelsson@gmail.com>
2024-08-20 11:54:44 +0100

    Remove extraneous /

--- cli/install.sh
+++ cli/install.sh
@@ -3,6 +3,6 @@ echo "Downloading binary from $URL"
 curl -L -o ./juce-tools $URL
 echo "Setting mode to executable"
 chmod +x ./juce-tools
-DEST=/usr/local/bin/
+DEST=/usr/local/bin
 echo "Installing binary to $DEST"
 mv ./juce-tools $DEST/juce-tools
