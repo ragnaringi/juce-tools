ff159250571263661a16acdd192d24a486176d3a
Ragnar Hrafnkelsson <ragnar.hrafnkelsson@gmail.com>
2025-09-05 13:51:28 +0100

    Fix logging output on Windows

--- cli/logger.go
+++ cli/logger.go
@@ -2,27 +2,55 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
+
+	"golang.org/x/sys/windows"
 )
 
-const (
-	colorReset  = "\033[0m"
-	colorRed    = "\033[31m"
-	colorGreen  = "\033[32m"
+var (
+	colorReset  = ""
+	colorRed    = ""
+	colorGreen  = ""
+	colorYellow = ""
+	colorBlue   = ""
+)
+
+func init() {
+	// Enable ANSI colors on Windows 10+
+	if runtime.GOOS == "windows" {
+		enableANSI()
+	}
+
+	colorGreen = "\033[32m"
 	colorYellow = "\033[33m"
-	colorBlue   = "\033[34m"
-)
+	colorRed = "\033[31m"
+	colorBlue = "\033[34m"
+	colorReset = "\033[0m"
+}
 
-// Always visible: success, warnings, errors.
+// enableANSI enables ANSI escape codes on Windows 10+ consoles.
+func enableANSI() {
+	stdout := windows.Handle(os.Stdout.Fd())
+	var mode uint32
+	if err := windows.GetConsoleMode(stdout, &mode); err != nil {
+		return
+	}
+	const ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004
+	_ = windows.SetConsoleMode(stdout, mode|ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+}
+
+// Log functions without icons
 func success(format string, args ...interface{}) {
-	fmt.Printf(colorGreen+"✓ "+format+colorReset+"\n", args...)
+	fmt.Printf(colorGreen+format+colorReset+"\n", args...)
 }
 
 func warn(format string, args ...interface{}) {
-	fmt.Printf(colorYellow+"⚠ "+format+colorReset+"\n", args...)
+	fmt.Printf(colorYellow+format+colorReset+"\n", args...)
 }
 
 func fail(format string, args ...interface{}) {
-	fmt.Printf(colorRed+"✗ "+format+colorReset+"\n", args...)
+	fmt.Printf(colorRed+format+colorReset+"\n", args...)
 }
 
 func notice(format string, args ...interface{}) {
