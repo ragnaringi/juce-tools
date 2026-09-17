93604f9005d38aa736c034990538f21ecfc1ee34
Ragnar Hrafnkelsson <ragnar.hrafnkelsson@gmail.com>
2026-09-17 10:24:03 +0100

    refactor: restructure files

---
+++ cli/internal/cli/app.go
@@ -0,0 +1,90 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func run(cmd *exec.Cmd) (bool, error) {
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(fmt.Sprint(err) + ": " + string(output))
+		return false, err
+	}
+	return true, nil
+}
+
+func Run(args []string) int {
+	flags := flag.NewFlagSet("juce", flag.ExitOnError)
+	flags.Parse(args)
+
+	if flags.NArg() == 0 {
+		fmt.Println("No arguments found")
+		return 1
+	}
+
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	// Find the project
+	project := NewProject(workingDirectory)
+	success("Found %s", filepath.Base(project.jucerFilePath))
+
+	// Locate JUCE
+	juce := NewJUCE(workingDirectory)
+
+	// Ensure Projucer is built before using it
+	if ok, err := juce.projucer.Build(); !ok {
+		fail("Failed to build Projucer: %v", err)
+	}
+
+	// Dispatch commands
+	switch flags.Arg(0) {
+	case "up":
+		success("Opening %s", project.name)
+		if _, err := juce.projucer.Open(project.jucerFilePath); err != nil {
+			panic(err)
+		}
+	case "clean":
+		if flags.Arg(1) == "--all" {
+			if _, err := juce.projucer.Clean(); err != nil {
+				panic(err)
+			}
+		}
+		if _, err := project.Clean(); err != nil {
+			panic(err)
+		}
+	case "export", "code":
+		success("Exporting %s", project.name)
+		if _, err := juce.projucer.Export(project.jucerFilePath); err != nil {
+			panic(err)
+		}
+
+		if flags.Arg(0) == "code" {
+			success("Opening %s", project.name+ideProjectExtension)
+			if _, err := project.Open(); err != nil {
+				panic(err)
+			}
+		}
+	case "build":
+		success("Exporting %s", project.name)
+		if _, err := juce.projucer.Export(project.jucerFilePath); err != nil {
+			panic(err)
+		}
+
+		success("Building %s", project.name+ideProjectExtension)
+		if _, err := project.Build(); err != nil {
+			panic(err)
+		}
+	default:
+		warn("Unknown command: %s", flags.Arg(0))
+		return 1
+	}
+
+	return 0
+}
