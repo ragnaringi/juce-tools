7ae83f505d9a8a25ed48367f1bd10191d9064def
Ragnar Hrafnkelsson <ragnar.hrafnkelsson@gmail.com>
2026-09-17 14:54:13 +0100

    refactor: introduce JUCE command context

--- cli/internal/cli/commands_juce.go
+++ cli/internal/cli/commands_juce.go
@@ -5,34 +5,46 @@ import (
 	"path/filepath"
 )
 
-func loadContext(workingDirectory string) (*JUCEProject, *JUCE, error) {
+type JUCECommandContext struct {
+	workingDirectory string
+	project          *JUCEProject
+	juce             *JUCE
+	projucer         *Projucer
+}
+
+func loadJUCECommandContext(workingDirectory string) (*JUCECommandContext, error) {
 	project, err := NewProject(workingDirectory)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	success("Found %s", filepath.Base(project.jucerFilePath))
 
 	juce, err := NewJUCE(workingDirectory)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	if ok, err := juce.projucer.Build(); !ok {
-		return nil, nil, fmt.Errorf("failed to build Projucer: %w", err)
+		return nil, fmt.Errorf("failed to build Projucer: %w", err)
 	}
 
-	return project, juce, nil
+	return &JUCECommandContext{
+		workingDirectory: workingDirectory,
+		project:          project,
+		juce:             juce,
+		projucer:         juce.projucer,
+	}, nil
 }
 
 func runUp(workingDirectory string) error {
-	project, juce, err := loadContext(workingDirectory)
+	ctx, err := loadJUCECommandContext(workingDirectory)
 	if err != nil {
 		return err
 	}
 
-	success("Opening %s", project.name)
-	if _, err := juce.projucer.Open(project.jucerFilePath); err != nil {
+	success("Opening %s", ctx.project.name)
+	if _, err := ctx.projucer.Open(ctx.project.jucerFilePath); err != nil {
 		return err
 	}
 
@@ -40,18 +52,19 @@ func runUp(workingDirectory string) error {
 }
 
 func runClean(workingDirectory string, args []string) error {
-	project, juce, err := loadContext(workingDirectory)
+	ctx, err := loadJUCECommandContext(workingDirectory)
 	if err != nil {
 		return err
 	}
 
+	success("Cleaning %s", ctx.project.name)
 	if len(args) > 0 && args[0] == "--all" {
-		if _, err := juce.projucer.Clean(); err != nil {
+		if _, err := ctx.projucer.Clean(); err != nil {
 			return err
 		}
 	}
 
-	if _, err := project.Clean(); err != nil {
+	if _, err := ctx.project.Clean(); err != nil {
 		return err
 	}
 
@@ -59,13 +72,13 @@ func runClean(workingDirectory string, args []string) error {
 }
 
 func runExport(workingDirectory string) error {
-	project, juce, err := loadContext(workingDirectory)
+	ctx, err := loadJUCECommandContext(workingDirectory)
 	if err != nil {
 		return err
 	}
 
-	success("Exporting %s", project.name)
-	if _, err := juce.projucer.Export(project.jucerFilePath); err != nil {
+	success("Exporting %s", ctx.project.name)
+	if _, err := ctx.projucer.Export(ctx.project.jucerFilePath); err != nil {
 		return err
 	}
 
@@ -73,18 +86,18 @@ func runExport(workingDirectory string) error {
 }
 
 func runCode(workingDirectory string) error {
-	project, juce, err := loadContext(workingDirectory)
+	ctx, err := loadJUCECommandContext(workingDirectory)
 	if err != nil {
 		return err
 	}
 
-	success("Exporting %s", project.name)
-	if _, err := juce.projucer.Export(project.jucerFilePath); err != nil {
+	success("Exporting %s", ctx.project.name)
+	if _, err := ctx.projucer.Export(ctx.project.jucerFilePath); err != nil {
 		return err
 	}
 
-	success("Opening %s", project.name+ideProjectExtension)
-	if _, err := project.Open(); err != nil {
+	success("Opening %s", ctx.project.name+ideProjectExtension)
+	if _, err := ctx.project.Open(); err != nil {
 		return err
 	}
 
@@ -92,18 +105,18 @@ func runCode(workingDirectory string) error {
 }
 
 func runBuild(workingDirectory string) error {
-	project, juce, err := loadContext(workingDirectory)
+	ctx, err := loadJUCECommandContext(workingDirectory)
 	if err != nil {
 		return err
 	}
 
-	success("Exporting %s", project.name)
-	if _, err := juce.projucer.Export(project.jucerFilePath); err != nil {
+	success("Exporting %s", ctx.project.name)
+	if _, err := ctx.projucer.Export(ctx.project.jucerFilePath); err != nil {
 		return err
 	}
 
-	success("Building %s", project.name+ideProjectExtension)
-	if _, err := project.Build(); err != nil {
+	success("Building %s", ctx.project.name+ideProjectExtension)
+	if _, err := ctx.project.Build(); err != nil {
 		return err
 	}
 
